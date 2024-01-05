d757ba211fa7812ff4138e8a8633c2494eeb0d67
denis.schatzmann@students.fhnw.ch <denis.schatzmann@students.fhnw.ch>
2024-01-05 15:22:18 +0100

    Ballerkennung rechte Linse effizienter mit Pytorch

--- helperClasses/tennis_ball_detection_pytorch.py
+++ helperClasses/tennis_ball_detection_pytorch.py
@@ -4,17 +4,22 @@ import numpy as np
 from tqdm import tqdm
 import cv2
 from numpy.lib import stride_tricks
+import matplotlib.pyplot as plt
+import torch
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+import cProfile
 
-
-class BallDetection:
+class BallDetectionPytorch:
 
     def __init__(self, svo_path):
         self.svo_path = svo_path
-        self.median_background_l = None
+        self.median_background_l = torch.tensor([]).to(device)
+
+
 
     def get_ball_by_frame(self, frameFrom, frameTo, return_video = False):
 
-        """Returns the ball position in a frame
+        """Returns the ball position in a frame of left camera
 
         Args:
             frameFrom (int): Frame to start from
@@ -31,10 +36,7 @@ class BallDetection:
             ball_pos = get_ball_by_frame(0, 100, 'path/to/svo/file.svo', median_background_l, depth_background_l)
         """
 
-        if self.median_background_l is None:
-            self.median_background_l = np.load('median_background_l.npy')
-
-
+        tensor_median_background_l = self.median_background_l
 
         zed = sl.Camera()
 
@@ -72,12 +74,12 @@ class BallDetection:
 
         detected_objects = sl.Objects()
         runtime_parameters = sl.RuntimeParameters()
+
         list_of_ball_positions = []
-        video = np.zeros((1080, 1920, 3, frameTo - frameFrom))
+        video = torch.zeros((1080, 1920, 3, frameTo - frameFrom))
+        zed.set_svo_position(frameFrom)
 
         for frame in tqdm(range(frameFrom, frameTo)):
-            zed.set_svo_position(frame)
-
             if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
                 # Get Images
                 zed.retrieve_image(current_frame_left, sl.VIEW.LEFT)
@@ -85,28 +87,33 @@ class BallDetection:
 
                 # Get Arrays
                 current_frame_left_data = current_frame_left.get_data()[:, :, :3]
+                tensor_current_frame_left_data = torch.from_numpy(current_frame_left_data).to(device)
 
                 # Get Moving Pixels
-                moving_left = current_frame_left_data - self.median_background_l
+                tensor_moving_left = (tensor_current_frame_left_data - tensor_median_background_l).to(device)
 
                 # Get Tennis Ball Position
-                tennis_ball_pos = self.detect_tennis_ball(moving_left, detected_objects, current_frame_left_data)
+                tensor_tennis_ball_pos = self.detect_tennis_ball(tensor_moving_left, detected_objects, tensor_current_frame_left_data).to(device)
 
                 if not return_video:
-                    list_of_ball_positions.append(tennis_ball_pos)
+                   list_of_ball_positions.append(tensor_tennis_ball_pos)
 
                 else:
-                    tennis_ball_bb = self.draw_bb(current_frame_left_data, tennis_ball_pos)
-                    frame_with_bb = tennis_ball_bb.copy()
-                    frame_with_bb[tennis_ball_bb != 255] = current_frame_left_data[tennis_ball_bb != 255]
-                    video[:, :, :, frame - frameFrom] = frame_with_bb
+                    tensor_tennis_ball_bb = self.draw_bb(tensor_current_frame_left_data, tensor_tennis_ball_pos)
+                    tensor_frame_with_bb = tensor_tennis_ball_bb.clone()
+                    tensor_mask = tensor_tennis_ball_bb != 255
+                    tensor_frame_with_bb[tensor_mask] = tensor_current_frame_left_data[tensor_mask]
+                    video[:, :, :, frame - frameFrom] = tensor_frame_with_bb
 
         if return_video:
-            np.save(f'videoRaw.npy', video)
             self.make_mp4(video)
             return video
         else:
-            return list_of_ball_positions
+            #print(f'shape of list_of_ball_positions: {list_of_ball_positions.shape}')
+            result_list = torch.cat(list_of_ball_positions, dim=0)
+            print(f'shape of result_list: {result_list.shape}')
+            result_list = torch.reshape(result_list, (int(result_list.shape[0]/2), 2))
+            return result_list
 
     @staticmethod
     def make_mp4(video, bildrate = 20, auflösung = (1920, 1080)):
@@ -119,15 +126,15 @@ class BallDetection:
 
         :return: The converted video.
         """
-
+        video_numpy = video.cpu().numpy()
         datestring = datetime.now().strftime("%Y-%m-%d-%H-%M")
         filename = f'renderedVideo-{datestring}.mp4'
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec
         video_komplett = cv2.VideoWriter(filename, fourcc, 20, (1920, 1080))  # Bildrate und Auflösung
 
         # Bilder zum Video hinzufügen
-        for bild in tqdm(range(video.shape[3])):
-            video_komplett.write(video[:, :, :, bild].astype(np.uint8))
+        for bild in tqdm(range(video_numpy.shape[3])):
+            video_komplett.write(video_numpy[:, :, :, bild].astype(np.uint8))
 
         # Video speichern
         video_komplett.release()
@@ -146,17 +153,17 @@ class BallDetection:
         imageBlue = image[:, :, 0]
         imageGreen = image[:, :, 1]
         imageRed = image[:, :, 2]
-        newRGBImage = np.zeros_like(image)
+        newRGBImage = torch.zeros_like(image)
         newRGBImage[:, :, 0] = imageRed
         newRGBImage[:, :, 1] = imageGreen
         newRGBImage[:, :, 2] = imageBlue
 
-        returnImage = newRGBImage.astype(np.uint8)
+        returnImage = newRGBImage.type(torch.uint8)
         return returnImage
 
 
     @staticmethod
-    def get_area_of_interest(image, detected_objects):
+    def get_area_of_interest(tensor_image, detected_objects):
         """
         Returns the area of interest for the image. The area of interest is the area between the lowest and the highest person in the image.
 
@@ -165,7 +172,7 @@ class BallDetection:
         :return: The area of interest as a mask.
         """
 
-        mask = np.zeros_like(image)
+        mask = torch.zeros_like(tensor_image)
 
         tiefstePersonSum = np.inf
         tiefstePersonLocation = [1080, 1920]
@@ -191,13 +198,13 @@ class BallDetection:
         höchstePersonLocation[0] += 100
         höchstePersonLocation[1] += 100
 
-        mask[int(tiefstePersonLocation[1]):int(höchstePersonLocation[1]),
-        int(tiefstePersonLocation[0]):int(höchstePersonLocation[0])] = 1
+        mask[max(0, int(tiefstePersonLocation[1])):max(0, int(höchstePersonLocation[1])),
+        max(0, int(tiefstePersonLocation[0])):max(0, int(höchstePersonLocation[0]))] = 1
 
         return mask
 
     @staticmethod
-    def find_yellow_areas(image, yellow_threshold = [(0, 180, 180), (200, 255, 255)]):
+    def find_yellow_areas(tensor_image, device, yellow_threshold = [(0, 180, 180), (200, 255, 255)]):
         """
         Finds the yellow areas in the image. The yellow areas are defined by the yellow_threshold.
         Attention: The ZED-Library returns BGR images and not RGB images.This function is therefore designed for BGR images.
@@ -207,13 +214,13 @@ class BallDetection:
 
         :return: A binary matrix with the yellow areas.
         """
-        lower, upper = np.array(yellow_threshold[0]), np.array(yellow_threshold[1])
-        mask = np.all(np.logical_and(lower <= image, image <= upper), axis=-1)
+        lower, upper = torch.tensor(yellow_threshold[0]).to(device), torch.tensor(yellow_threshold[1]).to(device)
+        mask = torch.all((lower <= tensor_image) & (tensor_image <= upper), dim=-1)
 
-        return mask.astype(int)
+        return mask.int()
 
     @staticmethod
-    def sliding_window(image, window_size = (7, 7)):
+    def sliding_window(tensor_image, window_size = (7, 7)):
         """
         Moves a window over the image and calculates the sum of the pixel values in the window.
 
@@ -222,23 +229,18 @@ class BallDetection:
 
         :return: A matrix with the sum of the pixel values in the window.
         """
-        image_height, image_width = image.shape[:2]
+        image_height, image_width = tensor_image.shape[0], tensor_image.shape[1]
         window_height, window_width = window_size
 
-        # Berechnung der neuen Shape und Strides
-        new_shape = (image_height - window_height + 1, image_width - window_width + 1, window_height, window_width)
-        new_strides = (image.strides[0], image.strides[1], image.strides[0], image.strides[1])
-
-        # Erstellung eines 'strided' Arrays, das die Fenster darstellt
-        strided_image = stride_tricks.as_strided(image, shape=new_shape, strides=new_strides)
+        windows = tensor_image.unfold(0, window_height, 1).unfold(1, window_width, 1)
 
         # Berechnung der Summe in jedem Fenster
-        window_sums = np.sum(strided_image, axis=(2, 3))
+        tensor_window_sums = windows.sum(dim=[2, 3])
 
-        return window_sums
+        return tensor_window_sums
 
     @staticmethod
-    def get_mask(index, image, detected_objects, bounding_box_extension = 30):
+    def get_mask(index, tensor_image, detected_objects, bounding_box_extension = 30):
         """
         Returns a mask for the bounding box of the detected object.
 
@@ -249,7 +251,7 @@ class BallDetection:
         :return: A mask for the bounding box of the detected object.
         """
 
-        mask = np.zeros_like(image)
+        mask = torch.zeros_like(tensor_image)
 
         bb = detected_objects.object_list[index].bounding_box_2d
 
@@ -263,7 +265,7 @@ class BallDetection:
 
         return mask
 
-    def detect_tennis_ball(self, moving_pixels, detected_objects, original_picture):
+    def detect_tennis_ball(self, tensor_moving_pixels, detected_objects, tensor_original_picture):
         """
         Detects the tennis ball in the image.
 
@@ -273,31 +275,41 @@ class BallDetection:
 
         :return: The position of the tennis ball in the image. (tuple): (x, y)
         """
+        tensor_original_picture_gpu = tensor_original_picture.to(device)
+        tensor_moving_pixels_gpu = tensor_moving_pixels.to(device)
 
-        diff_pixels = np.square(moving_pixels.astype(np.uint8)).sum(axis=2)
 
-        areaOfInterest = self.get_area_of_interest(diff_pixels, detected_objects)
-        filteredByInterest = diff_pixels * areaOfInterest
+        tensor_diff_pixels = torch.square(tensor_moving_pixels_gpu.type(torch.uint8)).sum(dim=2).to(device)
+        #diff_pixels = np.square(moving_pixels.astype(np.uint8)).sum(axis=2)
 
-        minPixelStrength = int(filteredByInterest.max() * 0.7)
-        diff_pixels_dsa = filteredByInterest.copy()
-        diff_pixels_dsa[diff_pixels_dsa < minPixelStrength] = 0
+        tensor_areaOfInterest = self.get_area_of_interest(tensor_diff_pixels, detected_objects).to(device)
+        tensor_filteredByInterest = (tensor_diff_pixels * tensor_areaOfInterest).to(device)
 
-        masked = diff_pixels_dsa.copy()
+        tensor_minPixelStrength = int(tensor_filteredByInterest.max() * 0.7)
+        tensor_diff_pixels_dsa = tensor_filteredByInterest.clone().to(device)
+        tensor_diff_pixels_dsa[tensor_diff_pixels_dsa < tensor_minPixelStrength] = 0
+
+        tensor_masked = tensor_diff_pixels_dsa.clone().to(device)
+
         for i in range(len(detected_objects.object_list)):
-            mask = self.get_mask(i, diff_pixels_dsa, detected_objects)
-            masked[mask == 1.0] = 0.0
+            mask = self.get_mask(i, tensor_diff_pixels_dsa, detected_objects).to(device)
+            tensor_masked[mask == 1.0] = 0.0
 
-        yellow_areas = self.find_yellow_areas(original_picture)
-        yellow_areas = yellow_areas * masked
+        tensor_yellow_areas = self.find_yellow_areas(tensor_original_picture_gpu, device).to(device)
+        tensor_yellow_areas = (tensor_yellow_areas * tensor_masked).to(device)
 
-        windows = self.sliding_window(yellow_areas, (7, 7))
+        tensor_windows = self.sliding_window(tensor_yellow_areas, (7, 7)).to(device)
 
-        tennisballs = np.unravel_index(np.argsort(windows, axis=None)[::-1], windows.shape)
-        return [tennisballs[0][0], tennisballs[1][0]]
+        max_index = torch.argmax(tensor_windows).to(device)
+        y_coord = max_index // tensor_windows.shape[1]
+        x_coord = max_index % tensor_windows.shape[1]
+
+        tensor_coordinates = torch.tensor([y_coord, x_coord]).to(device)
+
+        return tensor_coordinates
 
     @staticmethod
-    def draw_bb(image, center):
+    def draw_bb(tensor_image, tensor_center):
         """
         Draws a bounding box around the center of the tennis ball.
 
@@ -306,11 +318,11 @@ class BallDetection:
 
         :return: The image with the bounding box.
         """
-        bb = np.zeros_like(image)
-        bb[center[0] - 15:center[0] + 15, center[1] - 15:center[1] + 15] = 255
+        bb = torch.zeros_like(tensor_image).to(device)
+        bb[tensor_center[0] - 15:tensor_center[0] + 15, tensor_center[1] - 15:tensor_center[1] + 15] = 255
         return bb
 
-    def get_background(self, svo_path, calibration_frames = 120, skip_frames = 30):
+    def calculate_background(self, svo_path, calibration_frames = 120, skip_frames = 30):
         """
         Calculates the median background and the depth background of the left camera.
 
@@ -338,30 +350,40 @@ class BallDetection:
 
         runtime_parameters = sl.RuntimeParameters()
         temp_image_left = sl.Mat()
-        # %%
-        color_array_l = np.zeros((1080, 1920, 3, calibration_frames))
-        # %%
-        for i in tqdm(range(calibration_frames * skip_frames)):
-            zed.grab(runtime_parameters)
-            if i % skip_frames != 0:
-                continue
-            zed.retrieve_image(temp_image_left, sl.VIEW.LEFT)
-            current_frame_l = temp_image_left.get_data()[:, :, :3]
-            color_array_l[:, :, :, i // skip_frames] = current_frame_l
+        color_array_l = torch.zeros((1080, 1920, 3, calibration_frames)).to(device)
+        range_count = calibration_frames * skip_frames
+        for i in tqdm(range(range_count)):
+            if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+                if i % skip_frames != 0:
+                    continue
+                zed.retrieve_image(temp_image_left, sl.VIEW.LEFT)
+                current_frame_l = temp_image_left.get_data()[:, :, :3]
+                color_array_l[:, :, :, i // skip_frames] = torch.from_numpy(current_frame_l)
 
         # get median depth
-        median_depth_l = np.nanmedian(color_array_l, axis=3)
-        np.save('median_background_l.npy', median_depth_l)
+        median_depth_l = color_array_l.nanmedian(3).values
         self.median_background_l = median_depth_l
-
-        return median_depth_l
         zed.close()
+        return median_depth_l
 
-    def load_background(self, background_path):
-        """
-        Loads the previously calculated median background of the left camera.
+    def save_background(self, path = 'median_background.pt'):
+        '''
+        Saves the median background of a video to a local file.
 
-        :param background_path (str): The path to the background file. Has to be a .npy file.
-        """
-        self.median_background_l = np.load(background_path)
+        :param path (str): The path to the file. standard: 'median_background.pt'
+        '''
+
+        torch.save(self.median_background_l, path)
+
+    def load_background(self, path = 'median_background.pt'):
+        '''
+        Loads the median background of a video from a local file.
+
+        :param path (str): The path to the file. standard: 'median_background.pt'
+
+        :return: The median background.
+        '''
+
+        self.median_background_l = torch.load(path)
         return self.median_background_l
+
